53dd3f4f941abf63bd5cd3c9aed7b916c2bf5849
mdasif8393 <md.asif.8393@gmail.com>
2024-02-27 19:58:02 +0600

    done navbar hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeoSection";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
+      <HeroSection />
+      <Footer />
     </>
   );
 }
